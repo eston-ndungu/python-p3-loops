3224e4adc2c0539c73d498cd80f1ec83fe544ec2
ESTON NDUNGU <estonndungu1@gmail.com>
2024-08-31 13:48:35 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+        print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+   return [int ** 2 for int in int_list ]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range (1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
